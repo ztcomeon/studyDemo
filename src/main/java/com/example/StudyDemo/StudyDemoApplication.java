ed475570e40ce0bd23df2145b5c938f46d58fc1e
zhangtianxin <1582158644@qq.com>
2020-04-01 23:50:50 +0800

    注释循环依赖

--- src/main/java/com/example/StudyDemo/StudyDemoApplication.java
+++ src/main/java/com/example/StudyDemo/StudyDemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.StudyDemo;
 
-import com.example.StudyDemo.configuration.rely.RelyA;
-import com.example.StudyDemo.configuration.rely.RelyB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,13 +12,13 @@ public class StudyDemoApplication {
 //		Book book = (Book)run.getBean("book");
 //		System.out.println("Book name = " + book.getBookName());
 
-        RelyA relyA =(RelyA) run.getBean("relyA");
-        System.out.println(relyA);
-
-        RelyB relyB=(RelyB)run.getBean("relyB") ;
-        System.out.println(relyB);
-
-        run.close();
+//        RelyA relyA =(RelyA) run.getBean("relyA");
+//        System.out.println(relyA);
+//
+//        RelyB relyB=(RelyB)run.getBean("relyB") ;
+//        System.out.println(relyB);
+//
+//        run.close();
     }
 
 }
